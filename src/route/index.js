721f61604691778aeecf94b30450fa1033818537
struchek <leshavika2014@gmail.com>
2023-12-20 21:03:41 +0200

    end flexbox lesson

--- src/route/index.js
+++ src/route/index.js
@@ -211,7 +211,7 @@ router.get('/template-2', function (req, res) {
       {
         date: 'Oct 24',
         title: 'Latest post',
-        text: 'This is a wider card with supporting text below as a natural lead-in to additional content',
+        text: ' This is a wider card with supporting text below as a natural lead-in to additional contentThis is a wider card with supporting text below as a natural lead-in to additional content',
         category: [
           {
             id: 1,
@@ -242,6 +242,17 @@ router.get('/template-2', function (req, res) {
           {
             id: 3,
             text: 'Hot',
+          },
+        ],
+      },
+      {
+        date: 'Oct 30',
+        title: 'Trending topic',
+        text: 'This is a wider card with supporting text below as a natural lead-in to additional content',
+        category: [
+          {
+            id: 4,
+            text: 'Fashion',
           },
         ],
       },
